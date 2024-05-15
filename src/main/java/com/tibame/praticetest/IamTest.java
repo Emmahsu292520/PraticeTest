e61952d0b4f33a10fd823fd9bed5fdce6df41a65
ChihFang <jrfang2020@gmail.com>
2024-05-15 20:26:25 +0800

    測試分枝第一次

--- src/main/java/com/tibame/praticetest/IamTest.java
+++ src/main/java/com/tibame/praticetest/IamTest.java
@@ -3,7 +3,7 @@ package com.tibame.praticetest;
 public class IamTest {
 
 	public static void main(String[] args) {
-		System.out.println("芳芳芳芳芳慈慈慈慈慈慈學妹哈哈哈哈");
+		System.out.println("我在測試分枝分枝");
 
 	}
 
