c87f56dcde7a9a83c4aa8d212958d36afa5d2a77
Emma <down292520@gmail.com>
2024-05-15 16:24:16 +0800

    新增member2

---
+++ src/main/java/com/tibame/praticetest/member/Member2.java
@@ -0,0 +1,19 @@
+package com.tibame.praticetest.member;
+
+public class Member2 {
+	
+	private Integer memno;
+	private String memName;
+	public Integer getMemno() {
+		return memno;
+	}
+	public void setMemno(Integer memno) {
+		this.memno = memno;
+	}
+	public String getMemName() {
+		return memName;
+	}
+	public void setMemName(String memName) {
+		this.memName = memName;
+	}
+}
