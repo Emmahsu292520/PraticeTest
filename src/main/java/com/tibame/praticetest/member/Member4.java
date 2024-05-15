dd1a001ab1d94f94117adfd0a9a414317687e1b9
ChihFang <jrfang2020@gmail.com>
2024-05-15 21:55:54 +0800

    test 2 第三波一直失敗

--- src/main/java/com/tibame/praticetest/member/Member4.java
+++ src/main/java/com/tibame/praticetest/member/Member4.java
@@ -4,7 +4,7 @@ public class Member4 {
 	public static void main(String[] args) {
 		System.out.println("想吃酸白菜鍋也想吃酸菜魚");
 		System.out.println("我今天看到一隻黃色的魚 一口就把小龍蝦的身體咬碎");
-		System.out.println("pr 又消失了~");
+		System.out.println("pr 又消失了~~~");
 		
 	}
 	
