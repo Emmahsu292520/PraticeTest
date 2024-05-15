68ae12ba2b767934eb0ae98917301a63f4cc6be1
ChihFang <jrfang2020@gmail.com>
2024-05-15 21:48:43 +0800

    test 2 conflict

--- src/main/java/com/tibame/praticetest/member/Member4.java
+++ src/main/java/com/tibame/praticetest/member/Member4.java
@@ -3,6 +3,8 @@ package com.tibame.praticetest.member;
 public class Member4 {
 	public static void main(String[] args) {
 		System.out.println("想吃酸白菜鍋也想吃酸菜魚");
+		System.out.println("我今天看到一隻黃色的魚 一口就把小龍蝦的身體咬碎");
+		
 	}
 	
 
