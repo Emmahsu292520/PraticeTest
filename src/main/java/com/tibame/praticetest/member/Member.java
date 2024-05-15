61e5c35e517e7871bafab6472d86c11a4315ec84
Emma <down292520@gmail.com>
2024-05-15 16:17:51 +0800

    third commit

--- src/main/java/com/tibame/praticetest/member/Member.java
+++ src/main/java/com/tibame/praticetest/member/Member.java
@@ -1,5 +1,21 @@
 package com.tibame.praticetest.member;
 
 public class Member {
+	private Integer memno;
+	private String memName;
+	public Integer getMemno() {
+		return memno;
+	}
+	public void setMemno(Integer memno) {
+		this.memno = memno;
+	}
+	public String getMemName() {
+		return memName;
+	}
+	public void setMemName(String memName) {
+		this.memName = memName;
+	}
+	
+	
 
 }
