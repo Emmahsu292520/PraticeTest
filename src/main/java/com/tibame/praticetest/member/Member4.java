14901d90fb4645c61981daea9e8a8ba8583534b6
ChihFang <jrfang2020@gmail.com>
2024-05-15 22:06:17 +0800

    test 2 第五波

--- src/main/java/com/tibame/praticetest/member/Member4.java
+++ src/main/java/com/tibame/praticetest/member/Member4.java
@@ -5,7 +5,7 @@ public class Member4 {
 		System.out.println("想吃酸白菜鍋也想吃酸菜魚");
 
 		System.out.println("我今天看到一隻黃色的魚 一口就把小龍蝦的身體咬碎");
-		System.out.println("pr 又消失了~~~");
+		System.out.println("到底甚麼鬼");
 
 		System.out.println("眼睛好痠阿");
 
